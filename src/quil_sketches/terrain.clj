0cca07a6b8d292c605a03d795343bf16c4f2eb99
Jack Rusher <jack@rusher.com>
2014-05-16 16:07:56 +0200

    slight cleanup

--- src/quil_sketches/terrain.clj
+++ src/quil_sketches/terrain.clj
@@ -7,11 +7,9 @@
 (defn new-terrain [detail]
   (let [t-size (int (pow 2 detail))
         t-max  (- t-size 1)]
-    (-> (into [] (repeatedly t-size #(vec (repeat t-size 0.0))))
-        (assoc-in [0 0] t-max)
-        (assoc-in [t-max t-max] 0)
-        (assoc-in [t-max 0] (float (/ t-max 2)))
-        (assoc-in [0 t-max] (float (/ t-max 2))))))
+    (reduce (fn [t [x y v]] (assoc-in t [x y] v))
+            (into [] (repeatedly t-size #(vec (repeat t-size 0.0))))
+            [[0 0 t-max] [t-max t-max 0] [t-max 0 (float (/ t-max 2))] [0 t-max (float (/ t-max 2))]])))
 
 (defn filtered-average [nums]
   (let [valid     (remove (partial = -1) nums)
@@ -19,11 +17,8 @@
     (if (= num-valid 0) 0 (/ (reduce + valid) num-valid))))
 
 (defn square [terrain x y sz offset]
-  (assoc-in terrain [x y]
-            (+ offset (filtered-average [(get-in terrain [(- x sz) (- y sz)] -1)
-                                         (get-in terrain [(- x sz) (+ y sz)] -1)
-                                         (get-in terrain [(+ x sz) (- y sz)] -1)
-                                         (get-in terrain [(+ x sz) (+ y sz)] -1)]))))
+  (assoc-in terrain [x y] (+ offset (filtered-average (for [[op1 op2] [[- -] [- +] [+ -] [+ +]]]
+                                                        (get-in terrain [(op1 x sz) (op2 y sz)] -1))))))
 
 (defn diamond [terrain x y sz offset]
   (assoc-in terrain [x y]
@@ -32,20 +27,19 @@
                                          (get-in terrain [(- x sz) y] -1)
                                          (get-in terrain [(+ x sz) y] -1)]))))
 
-(defn generate
-  ([terrain roughness] (generate terrain roughness (count terrain)))
-  ([terrain roughness sz]
-     (let [half (/ sz 2)]
-       (if (> 1 half)
-         terrain
-         (let [t-max (count terrain)
-               scale (* roughness sz)]
-           (generate (reduce (fn [t [y x]] (diamond t x y half (random (- scale) scale)))
-                             (reduce (fn [t [x y]] (square t x y half (random (- scale) scale)))
-                                     terrain
-                                     (for [x (range half t-max sz) y (range half t-max sz)] [x y]))
-                             (for [y (range 0 t-max half) x (range (mod (+ y half) sz) t-max sz)] [y x]))
-                     roughness (/ sz 2)))))))
+(defn generate [terrain roughness]
+  (loop [terrain terrain roughness roughness sz (count terrain)]
+    (let [half (/ sz 2)]
+      (if (> 1 half)
+        terrain
+        (let [t-max (count terrain)
+              scale (* roughness sz)]
+          (recur (reduce (fn [t [y x]] (diamond t x y half (random (- scale) scale)))
+                         (reduce (fn [t [x y]] (square t x y half (random (- scale) scale)))
+                                 terrain
+                                 (for [x (range half t-max sz) y (range half t-max sz)] [x y]))
+                         (for [y (range 0 t-max half) x (range (mod (+ y half) sz) t-max sz)] [y x]))
+                 roughness (/ sz 2)))))))
 
 (defn color-rect [top bottom c]
   (when (>= (.y bottom) (.y top))
@@ -76,11 +70,10 @@
           (color-rect water bottom (color 50 150 200 (* 256 0.15))))))))
 
 (defn setup []
-  (frame-rate 10)
+  (frame-rate 2)
   (set-state! :terra (atom (generate (new-terrain 8) 0.7))))
 
 (defn draw []
-  (frame-rate 2)
   (apply background (hex-to-rgb "#C1EDF2"))
   (draw-terrain @(state :terra)))
 
